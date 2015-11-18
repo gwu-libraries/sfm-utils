076cc6a996c0b7edd4484af378903b24a0ff41b3
Justin Littman <justinlittman@gwu.edu>
2015-11-18 13:08:01 -0500

    More tweaks for setup.

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setup(
     test_suite='tests',
     scripts=['sfmutils/stream_consumer.py'],
     install_requires=['requests>=2.7.0',
-                      'warcprox-dev',
+                      'warcprox-gwu',
                       'pika>=0.10.0',
                       'supervisor>=3.1.3'],
     tests_require=['mock>=1.3.0'],
-    dependency_links=['git+https://github.com/gwu-libraries/warcprox.git@master#egg=warcprox-dev'],
+    dependency_links=['git+https://github.com/gwu-libraries/warcprox.git@master#egg=warcprox-gwu'],
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
