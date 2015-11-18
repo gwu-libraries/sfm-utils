e76e4f6b5531aba3b6dbf2f6ab87297dc2a4d2b0
Justin Littman <justinlittman@gwu.edu>
2015-11-18 12:52:17 -0500

    Fiddling with setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setup(
     test_suite='tests',
     scripts=['sfmutils/stream_consumer.py'],
     install_requires=['requests>=2.7.0',
-                      'warcprox',
+                      'warcprox-dev',
                       'pika>=0.10.0',
                       'supervisor>=3.1.3'],
     tests_require=['mock>=1.3.0'],
-    dependency_links=['git+https://github.com/gwu-libraries/warcprox.git@master#egg=warcprox'],
+    dependency_links=['git+https://github.com/gwu-libraries/warcprox.git@master#egg=warcprox-dev'],
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
