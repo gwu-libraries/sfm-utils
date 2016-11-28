0606325a5ed7ae04990cdb68f31a7bea83ed4251
Justin Littman <justinlittman@gwu.edu>
2016-11-27 20:53:27 -0500

    refs sfm 532. Adds creating sfm user and group to base image."

--- docker/base/setup_reqs.sh
+++ docker/base/setup_reqs.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+groupadd -r sfm --gid=$SFM_GID && useradd -r -m -g sfm --uid=$SFM_UID sfm
+
+export CONTAINER_DIR=/sfm-data/containers/$HOSTNAME
+if [ ! -d $CONTAINER_DIR ]; then
+    echo "Creating container directory"
+    mkdir -p $CONTAINER_DIR
+    chown sfm:sfm $CONTAINER_DIR
+fi
+
 echo "Upgrading common.txt"
 pip install -r requirements/common.txt --upgrade
 
