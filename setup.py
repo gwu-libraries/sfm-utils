1401518573593985a8ba02ff66bb08abedc17174
Justin Littman <justinlittman@gwu.edu>
2017-10-23 11:41:57 -0400

    Bumped version to 1.11.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmutils',
-    version='1.10.0',
+    version='1.11.0',
     url='https://github.com/gwu-libraries/sfm-utils',
     author='Justin Littman',
     author_email='justinlittman@gmail.com',
